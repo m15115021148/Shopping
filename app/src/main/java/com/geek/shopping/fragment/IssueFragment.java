5dedc52d440d9baeec1bcf11ade552926dc04e95
Romantic <m15115021148@163.com>
2019-05-21 19:05:57 +0800

    issue banner click listener

--- app/src/main/java/com/geek/shopping/fragment/IssueFragment.java
+++ app/src/main/java/com/geek/shopping/fragment/IssueFragment.java
@@ -4,14 +4,19 @@ import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.geek.shopping.R;
 import com.geek.shopping.adapter.IssueAdapter;
 import com.geek.shopping.application.MyApplication;
 import com.geek.shopping.config.ConfigUtil;
 import com.geek.shopping.database.entity.ProductModel;
+import com.geek.shopping.ui.IssueActivity;
 import com.geek.shopping.ui.IssueDetailActivity;
 import com.geek.shopping.view.GlideImageLoader;
+import com.geek.shopping.view.recyclerview.EmptyWrapper;
 import com.geek.shopping.view.recyclerview.HeaderAndFooterWrapper;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
@@ -31,7 +36,6 @@ public class IssueFragment extends BaseFragment implements IssueAdapter.OnIssueC
     private IssueAdapter mAdapter;
     private HeaderAndFooterWrapper mWrapper;
     private Banner mBanner;
-
 
     @Override
     protected int setContentView() {
@@ -62,11 +66,22 @@ public class IssueFragment extends BaseFragment implements IssueAdapter.OnIssueC
         super.onStart();
         List<ProductModel> list = MyApplication.getInstance().mDbIssue.getUserData(ConfigUtil.USER_ID);
 
-        if (mBanner != null){
-            mBanner.setImages(getBannerData(list));
-            mBanner.start();
+        if (list != null && list.size()>0){
+            if (mBanner != null){
+                mBanner.setImages(getBannerData(list));
+                mBanner.start();
+            }
+
+            mWrapper.removeFootView();
+            mAdapter.setData(list);
+            mWrapper.notifyDataSetChanged();
+        }else {
+            mWrapper.removeHeaderView();
+            mAdapter.setData(new ArrayList<ProductModel>());
+
+            setEmptyView();
         }
-        mAdapter.setData(list);
+
     }
 
     @Override
@@ -113,5 +128,28 @@ public class IssueFragment extends BaseFragment implements IssueAdapter.OnIssueC
         Intent intent = new Intent(getContext(), IssueDetailActivity.class);
         intent.putExtra("ProductModel",mAdapter.getData().get(position));
         startActivity(intent);
+    }
+
+    private void setEmptyView(){
+        View view = getLayoutInflater().inflate(R.layout.issue_empty_layout,null);
+
+        TextView issue = view.findViewById(R.id.issue);
+
+        issue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), IssueActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        view.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        ));
+        view.setPadding(0,500,0,0);
+        mWrapper.removeFootView();
+        mWrapper.addFootView(view);
+
+        mRecyclerView.setAdapter(mWrapper);
     }
 }
--- app/src/main/java/com/geek/shopping/fragment/MySelfFragment.java
+++ app/src/main/java/com/geek/shopping/fragment/MySelfFragment.java
@@ -1,6 +1,7 @@
 package com.geek.shopping.fragment;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -9,8 +10,12 @@ import com.geek.shopping.R;
 import com.geek.shopping.application.MyApplication;
 import com.geek.shopping.config.ConfigUtil;
 import com.geek.shopping.database.entity.ProductModel;
+import com.geek.shopping.database.entity.UserModel;
 import com.geek.shopping.ui.AboutActivity;
+import com.geek.shopping.ui.AccountActivity;
 import com.geek.shopping.ui.MyIssueActivity;
+import com.geek.shopping.util.ImageUtil;
+import com.geek.shopping.util.PreferencesUtil;
 import com.geek.shopping.util.ToastUtil;
 import com.geek.shopping.view.MyImageView;
 
@@ -65,12 +70,15 @@ public class MySelfFragment extends BaseFragment implements View.OnClickListener
             mIssue.setText(String.format(getString(R.string.my_self_issue_number),list.size()));
         }
         mOrder.setText(String.format(getString(R.string.my_self_order_number),0));
+
+        updateUser();
     }
 
     @Override
     public void onClick(View v) {
         if (v == mHeader){
-
+            Intent intent = new Intent(getContext(), AccountActivity.class);
+            startActivity(intent);
         }
         if (v == mReList.get(0)){
             Intent intent = new Intent(getContext(), MyIssueActivity.class);
@@ -86,5 +94,14 @@ public class MySelfFragment extends BaseFragment implements View.OnClickListener
             Intent intent = new Intent(getContext(), AboutActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void updateUser(){
+        UserModel model = MyApplication.getInstance().mDbUser.getUserData(PreferencesUtil.getStringData(getContext(), ConfigUtil.KEY_PHONE));
+        if (model == null)return;
+        ImageUtil.loadCircleImage(
+                getContext(),model.getHeaderImg(),R.drawable.head_defaut,R.drawable.head_defaut,mHeader
+        );
+        mNickName.setText(TextUtils.isEmpty(model.getName())?"未设置":model.getName());
     }
 }
